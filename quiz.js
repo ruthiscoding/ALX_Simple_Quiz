e1285996ac30ffefa0a94e02c1470575db013cd1
Ruth Udoh <ruthudohhappiness@gmail.com>
2024-07-16 20:07:49 +0100

    created a quiz

---
+++ quiz.js
@@ -0,0 +1,14 @@
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
+function checkAnswer() {
+  var correctAnswer = "4";
+
+  var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+  if (userAnswer === correctAnswer) {
+    document.getElementById("feedback").textContent = "Correct! Well done.";
+  } else {
+    document.getElementById("feedback").textContent =
+      "That's incorrect. Try again!";
+  }
+}
